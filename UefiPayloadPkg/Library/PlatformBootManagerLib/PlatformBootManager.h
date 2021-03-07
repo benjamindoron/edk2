b151a418ed1df6f70fddbb54dcba1f3363231f1f
Benjamin Doron <benjamin.doron00@gmail.com>
2021-03-07 00:37:28 -0500

    UefiPayloadPkg/PlatformBootManagerLib: Remove stale FV boot options
    Ported from OvmfPkg. These may be present if SMMSTORE region is marked
    as "PRESERVE" in coreboot's FMAP, and the user reflashes the firmware.
    
    Signed-off-by: Benjamin Doron <benjamin.doron00@gmail.com>

--- UefiPayloadPkg/Library/PlatformBootManagerLib/PlatformBootManager.h
+++ UefiPayloadPkg/Library/PlatformBootManagerLib/PlatformBootManager.h
@@ -29,6 +29,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include <Library/CapsuleLib.h>
 #include <Library/HobLib.h>
 #include <Library/Tcg2PhysicalPresenceLib.h>
+#include <Protocol/FirmwareVolume2.h>
 #include <Protocol/SmmAccess2.h>
 
 typedef struct {
