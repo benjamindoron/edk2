5a560f52e11b4c37a0d7e974a5cc287900bb62a1
Benjamin Doron <benjamin.doron00@gmail.com>
2023-04-01 16:41:05 -0400

    MdeModulePkg/Core: Process library constructors as soon as possible
    A debug library stack linked to the PEI core might use memory services,
    so initialise these first. For instance, this is required by
    dependencies of MinPlatform's PeiSerialPortLibSpiFlash.
    
    It appears that the DXE core's library constructors cannot be
    executed earlier anymore. If possible, this would enable printing
    debug messages while initialising GCD services.
    
    Signed-off-by: Benjamin Doron <benjamin.doron00@gmail.com>

--- MdeModulePkg/Core/Pei/PeiMain/PeiMain.c
+++ MdeModulePkg/Core/Pei/PeiMain/PeiMain.c
@@ -408,14 +408,14 @@ PeiCore (
   SetPeiServicesTablePointer ((CONST EFI_PEI_SERVICES **)&PrivateData.Ps);
 
   //
-  // Initialize libraries that the PEI Core is linked against
-  //
-  ProcessLibraryConstructorList (NULL, (CONST EFI_PEI_SERVICES **)&PrivateData.Ps);
-
-  //
   // Initialize PEI Core Services
   //
   InitializeMemoryServices (&PrivateData, SecCoreData, OldCoreData);
+
+  //
+  // Initialize libraries that the PEI Core is linked against
+  //
+  ProcessLibraryConstructorList (NULL, (CONST EFI_PEI_SERVICES **)&PrivateData.Ps);
 
   //
   // Update performance measurements
